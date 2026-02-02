a5451802e251883df61b86814ebf661de2ab4cef
soyacen <soyacen@outlook.com>
2026-02-02 22:22:25 +0800

    refactor: remove `ContextFunc` parameter from `Server` and `Client` middleware functions.

--- middleware/otelgoose/middleware.go
+++ middleware/otelgoose/middleware.go
@@ -1,7 +1,6 @@
 package otelgoose
 
 import (
-	"context"
 	"net/http"
 
 	"github.com/soyacen/goose"
@@ -11,9 +10,7 @@ import (
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
-type ContextFunc func(ctx context.Context) context.Context
-
-func Server(contextFunc ContextFunc) server.Middleware {
+func Server() server.Middleware {
 	return func(response http.ResponseWriter, request *http.Request, invoker http.HandlerFunc) {
 		var operation string
 		routeInfo, ok := goose.ExtractRouteInfo(request.Context())
@@ -26,7 +23,7 @@ func Server(contextFunc ContextFunc) server.Middleware {
 	}
 }
 
-func Client(contextFunc ContextFunc) client.Middleware {
+func Client() client.Middleware {
 	return func(cli *http.Client, request *http.Request, invoker client.Invoker) (*http.Response, error) {
 		cli.Transport = otelhttp.NewTransport(cli.Transport)
 		return invoker(cli, request)
